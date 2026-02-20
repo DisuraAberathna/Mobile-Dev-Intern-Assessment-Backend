108b30ba3de13d9e28cafde7048150f227c204a0
Disura Aberathna <disura2005@gmail.com>
2026-02-20 13:18:27 +0530

    gpt route initialized

--- src/route/api.route.js
+++ src/route/api.route.js
@@ -1,10 +1,12 @@
 import express from "express";
 import authRoute from "./auth.route.js";
 import courseRoute from "./course.route.js";
+import gptRoute from "./gpt.route.js";
 
 const apiRoutes = express();
 
 apiRoutes.use("/auth", authRoute);
 apiRoutes.use("/course", courseRoute);
+apiRoutes.use("/gpt", gptRoute);
 
 export default apiRoutes;
---
+++ src/route/gpt.route.js
@@ -0,0 +1,5 @@
+import express from "express";
+
+const gptRoute = express();
+
+export default gptRoute;
