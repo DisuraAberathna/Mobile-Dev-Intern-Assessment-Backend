8c107b242b125f60f4203fe3f46c8b6c64ade767
Disura Aberathna <disura2005@gmail.com>
2026-02-20 22:09:03 +0530

    implement business logic register

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -1,3 +1,44 @@
-export const register=()=>{};
+import dotenv from "dotenv";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../model/user.model.js";
 
-export const login=()=>{};
+dotenv.config();
+
+const secret = process.env.JWT_SECRET;
+
+export const register = async (req, resp) => {
+    try {
+        const { name, username, password, role } = req.body;
+
+        const foundUser = await User.findOne({ username });
+
+        if (foundUser) {
+            return resp.status(409).json({ message: "User already exists with this email address!" });
+        }
+
+        const hashedPassword = bcrypt.hash(password, 10);
+
+        const user = new User({
+            name,
+            username,
+            password: hashedPassword,
+            role
+        })
+
+        await user.save();
+
+        const token = generateToken(user._id, user.role);
+
+        return resp.status(201).json({ message: "User registration successful", token })
+    } catch (error) {
+        console.log("Registration failed : ", error);
+        return resp.status(500).json({ message: "Server error, Registraion failed!" });
+    }
+};
+
+export const login = () => { };
+
+const generateToken = (id, role) => {
+    return jwt.sign({ id, role }, secret, { expiresIn: '30d' });
+};
