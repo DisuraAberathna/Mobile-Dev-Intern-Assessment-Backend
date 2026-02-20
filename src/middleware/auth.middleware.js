8eb36d50fa290ea897fa2d7ae5c066e6de320e06
Disura Aberathna <disura2005@gmail.com>
2026-02-20 19:43:34 +0530

    Update auth.middleware.js

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -5,23 +5,29 @@ dotenv.config();
 
 const secret = process.env.JWT_SECRET;
 
-const auth = async (req, resp, next) => {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
+export const protect = async (req, resp, next) => {
+    const token = req.headers.authorization?.split(' ')[1];
 
     if (!token) {
         return resp.status(401).json({ message: "Auth token missing" });
     }
 
-    jwt.verify(token, secret, (error, decoded) => {
-        if (error) {
-            return resp.status(403).json({ message: "Invalid or expired token" });
-        }
-
+    try {
+        const decoded = jwt.verify(token, secret);
         req.user = decoded;
-
         next();
-    });
+    } catch (error) {
+        console.log("Auth validation failed : ", error);
+        return resp.status(401).json({ message: "Invalid or expired token" });
+    }
 };
 
-export default auth;
+export const authorize = (...roles) => {
+    return (req, resp, next) => {
+        if (!roles.includes(req.user.role)) {
+            return resp.status(403).json({ message: `User role ${req.user.role} is not authorized to access this route` });
+        }
+
+        next();
+    };
+};
