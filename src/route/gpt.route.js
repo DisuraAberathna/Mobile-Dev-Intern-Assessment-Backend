b1ff57d1aaf868b016117cbc5be11516c10c88bd
Disura Aberathna <disura2005@gmail.com>
2026-02-20 20:11:25 +0530

    gpt controller initialized

--- src/route/gpt.route.js
+++ src/route/gpt.route.js
@@ -1,5 +1,9 @@
 import express from "express";
+import { authorize, protect } from "../middleware/auth.middleware.js";
+import { getRecommendations } from "../controller/gpt.controller.js";
 
 const gptRoute = express();
+
+gptRoute.post("/recommend", protect, authorize("student"), getRecommendations);
 
 export default gptRoute;
