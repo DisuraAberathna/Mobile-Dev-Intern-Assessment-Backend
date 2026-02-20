ec0c9c09a8df50e80af99e3eb94abf8f610cf240
Disura Aberathna <disura2005@gmail.com>
2026-02-20 20:08:36 +0530

    auth controller initialized

--- src/route/auth.route.js
+++ src/route/auth.route.js
@@ -1,5 +1,10 @@
 import express from "express";
+import { registerRules, validate } from "../middleware/validator.middleware.js";
+import { login, register } from "../controller/auth.controller.js";
 
 const authRoute = express();
+
+authRoute.post("/register", registerRules, validate, register);
+authRoute.post("/login", login);
 
 export default authRoute;
