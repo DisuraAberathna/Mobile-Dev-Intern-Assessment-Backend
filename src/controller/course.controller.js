9582c3b989bfec57bead441a2119363c4b7d4d4c
Disura Aberathna <disura2005@gmail.com>
2026-02-21 12:46:21 +0530

    get all courses method immplement

--- src/controller/course.controller.js
+++ src/controller/course.controller.js
@@ -1,4 +1,16 @@
-export const getAllCourses = () => { };
+import Course from "../model/course.model.js";
+
+export const getAllCourses = async (req, resp) => {
+    try {
+        const courses = await Course.find().populate("instructor", "name");
+
+        return resp.status(200).json({ courses });
+    } catch (error) {
+        console.log("Get all courses failed : ", error);
+        return resp.status(500).json({ message: "Server error, Get all courses failed!" });
+
+    }
+};
 
 export const getEnrolledCourses = () => { };
 
