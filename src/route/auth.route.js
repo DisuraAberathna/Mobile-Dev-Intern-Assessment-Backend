90de1946c3e442cad37927914f911a53d1017528
Disura Aberathna <disura2005@gmail.com>
2026-02-25 20:01:42 +0530

    Update auth.route.js

--- src/route/auth.route.js
+++ src/route/auth.route.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { loginRules, registerRules, validate } from "../middleware/validator.middleware.js";
-import { login, register } from "../controller/auth.controller.js";
+import { login, register, getProfile } from "../controller/auth.controller.js";
+import { protect } from "../middleware/auth.middleware.js";
 
 const authRoute = express();
 
 authRoute.post("/register", registerRules, validate, register);
 authRoute.post("/login", loginRules, validate, login);
+authRoute.get("/profile", protect, getProfile);
 
 export default authRoute;
